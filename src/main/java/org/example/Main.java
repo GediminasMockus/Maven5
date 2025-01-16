ff6fda0a20f57eab7b00307cb2450eeb886886f4
GediminasM <gedmocss@gmail.com>
2025-01-16 18:39:25 +0200

    fixed task nr1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,47 +3,27 @@ package org.example;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.util.StdDateFormat;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("json!");
 
-        Person person1 = new Person("Jonas",25,"jonasjonaitis@gmail.com");
-        Person person2 = new Person("Antanas",30,"antanasantanaitis@gmail.com");
-        Person person3 = new Person("Petras",45,"petraspetraitis@gmail.com");
-        Person[] office ={person1,person2,person3};
-
-
-        Company company = new Company("Splius","Tilzes g. 52","Siauliai");
+        Person person = new Person("Jonas", 25, "jonasjonaitis@gmail.com");
 
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        objectMapper.setDateFormat(new StdDateFormat().withColonInTimeZone(true));
 
-        File outputJson = new File("src/main/java/org/example/outputJson.json");
-        objectMapper.writeValue(outputJson, office);
-
-        File input = new File("src/main/java/org/example/inputJson.json");
-
-        if (!input.exists() || input.length() == 0) {
-            System.out.println("Failas neegzistuoja arba yra tuščias. Įrašome numatytąjį turinį.");
-            objectMapper.writeValue(input, office);
-        }
+        File result = new File("src/main/java/org/example/rezultatas.json");
+        objectMapper.writeValue(result, person);
 
         try {
-            Company companyFromJson = objectMapper.readValue(input, Company.class);
-            System.out.println("Nuskaityti duomenys: " + companyFromJson);
+            Person personFromFile = objectMapper.readValue(result, Person.class);
+            System.out.println("Nuskaityti duomenys: " + personFromFile);
         } catch (IOException e) {
             System.err.println("Klaida nuskaitant failą: " + e.getMessage());
         }
 
-
-
     }
-
 }
--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -37,4 +37,13 @@ public class Person {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
